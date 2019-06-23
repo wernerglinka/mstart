613ea3c6d193e898cf8e9d922dc81a0726ccf144
werner glinka <werner@bussedesign.com>
2019-06-23 18:24:58 -0500

    added gulp prod command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const util = require('gulp-util');
+const gulpif = require('gulp-if');
 
 // Metalsmith build site process
 const metalsmith = require('./gulp/metalsmith');
@@ -8,6 +10,11 @@ const buildScripts = require('./gulp/buildNewScripts');
 const getIcons = require('./gulp/getIcons');
 const optimizeImages = require('./gulp/optimizeImages');
 const createResponsiveImages = require('./gulp/createResponsiveImages');
+
+// for "gulp", "util.env.production" will be undefined,"!!util.env.production" will coerce to boolean true
+// for "gulp --production", "util.env.production" will be true
+// source: https://j11y.io/javascript/truthy-falsey/
+const isProduction = !!util.env.production;
 
 // Function to reload the browser
 function reload(done) {
@@ -18,25 +25,26 @@ function reload(done) {
 // Function to watch all relevant source files and update browser accordingly
 // this function is only used during site development
 function watchSite(done) {
-  browserSync.init({
-    server: {
-      baseDir: './site/',
-    },
-  });
+  if (!isProduction) {
+    browserSync.init({
+      server: {
+        baseDir: './site/',
+      },
+    });
 
-  gulp.watch(
-    'src/scripts/**/*.js',
-    gulp.series(buildStyles, buildScripts, metalsmith, reload)
-  );
-  gulp.watch(
-    'src/styles/**/*.scss',
-    gulp.series(buildStyles, buildScripts, metalsmith, reload)
-  );
-  gulp.watch(
-    ['src/content/*.html', 'src/content/*.njk'],
-    gulp.series(buildStyles, buildScripts, metalsmith, reload)
-  );
-
+    gulp.watch(
+      'src/scripts/**/*.js',
+      gulp.series(buildStyles, buildScripts, metalsmith, reload)
+    );
+    gulp.watch(
+      'src/styles/**/*.scss',
+      gulp.series(buildStyles, buildScripts, metalsmith, reload)
+    );
+    gulp.watch(
+      ['src/content/*.html', 'src/content/*.njk'],
+      gulp.series(buildStyles, buildScripts, metalsmith, reload)
+    );
+  }
   done();
 }
 
@@ -49,5 +57,4 @@ const buildSite = gulp.series(
   metalsmith,
   watchSite
 );
-
 exports.default = buildSite;
